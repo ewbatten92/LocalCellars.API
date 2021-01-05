83802d3708a5bc12cd793daa417ac72ab64a7d89
ewbatten <ewbatten@gmail.com>
2021-01-04 20:38:39 -0500

    first commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using LocalCellars.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocalCellars.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options){}
+
+        public DbSet<Value> MyProperty { get; set; }
+    }
+}
---
+++ Models/Value.cs
@@ -0,0 +1,8 @@
+namespace LocalCellars.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+    }
+}
